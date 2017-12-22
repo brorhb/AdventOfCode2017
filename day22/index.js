8db43e6e833d018ca20f88ff1e305f34dba47042
brorhb <brorbrurberg@me.com>
2017-12-22 13:12:47 +0100

    day 22 part 1 :santa:

---
+++ day22/index.js
@@ -0,0 +1,84 @@
+let input = `..######.###...######...#
+.##..##.#....#..##.#....#
+.##.#....###..##.###.#.#.
+#.#.###.#####.###.##.##.#
+.###.#.#.###.####..##.###
+..####.##..#.#.#####...##
+....##.###..#.#..#...####
+.#.##.##.#..##...##.###..
+.######..#..#.#####....##
+###.##.###.########...###
+.#.#.#..#.##.#..###...#..
+.#.##.#.####.#.#.....###.
+##..###.###..##...#.##.##
+##.#.##..#...##...#...###
+##..#..###.#..##.#.#.#.#.
+.##.#####..##....#.#.#..#
+..#.######.##...#..#.##..
+#.##...#.#....###.#.##.#.
+.#..#.#.#..#.####..#.####
+.##...##....##..#.#.###..
+..##.#.#.##..##.#.#....#.
+###.###.######.#.########
+..#.####.#.#.##..####...#
+#.##..#.#.####...#..#..##
+###.###.#..##..#.###....#`
+
+/*input = `..#
+#..
+...`*/
+let directions = {
+  'up': [-1, 0],
+  'down': [1, 0],
+  'left': [0, -1],
+  'right': [0, 1]
+}
+let dir = 'up'
+let pos = [0, 0]
+let infected = {}
+let burstCausedInfection = 0
+var arr = input.split(/\n/).map((row) => row.split(''))
+var offset_y = Math.floor(arr.length / 2)
+var offset_x = Math.floor(arr[0].length / 2)
+arr.forEach((row, y) => row.forEach((col, x) => {
+  if (col === '#') {
+    infected[`${y - offset_y},${x - offset_x}`] = true
+  }
+}))
+let loops = 10000
+
+function move (d) {
+  if (d === 'left') {
+    if (dir === 'up') dir = 'left'
+    else if (dir === 'left') dir = 'down'
+    else if (dir === 'down') dir = 'right'
+    else if (dir === 'right') dir = 'up'
+  } 
+  else if (d === 'right') {
+    if (dir === 'up') dir = 'right'
+    else if (dir === 'right') dir = 'down'
+    else if (dir === 'down') dir = 'left'
+    else if (dir === 'left') dir = 'up'
+  }
+}
+
+function part1 () {
+  loops = 10000
+  while (loops--) {
+    let _pos = `${pos[0]},${pos[1]}`
+    if (infected[_pos] === true) {
+      infected[_pos] = false
+      move('right')
+    } else {
+      infected[_pos] = true
+      burstCausedInfection++
+      move('left')
+    }
+    pos[0] += directions[dir][0]
+    pos[1] += directions[dir][1]
+  }
+  console.log('infected', Object.keys(infected).length)
+  console.log(burstCausedInfection + ' bursts have caused an infection')
+}
+
+part1()
